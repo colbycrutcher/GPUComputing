f15060a96b7c190f845c6e45c45fa5b744876276
Colby <colbycrutcher@gmail.com>
2026-01-14 11:18:48 -0800

    init kernels

--- hw2/lab0.cu
+++ hw2/lab0.cu
@@ -1,10 +1,26 @@
-__global__ void kernell(int * a){
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    a[idx] = idx;
+include <stdio.h>
+
+//kernel1
+__global__ void kernel1(int *a)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    a[idx] = 7;
 }
 
-__global__ void kernell(int* b){
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    b[idx] = b[idx] * 2;
-    
+
+//Kernel 2
+__global__ void kernel2(int *a)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    a[idx] = blockIdx.x;
+}
+
+
+
+
+//Kernel 3
+__global__ void kernel3(int *a)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    a[idx] = threadIdx.x;
 }
