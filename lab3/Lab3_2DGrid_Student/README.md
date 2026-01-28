fe071828faaac0a28a856068839d8a678a01ffb0
Colby Crutcher <94940284+colbycrutcher@users.noreply.github.com>
2026-01-28 13:11:14 -0800

    finish lab

---
+++ lab3/Lab3_2DGrid_Student/README.md
@@ -0,0 +1,11 @@
+# Colby Crutcher, Riley Rudolfo
+
+# CSCD Lab 3
+
+# Running this code
+
+1. Run 'make' tob build the 2dGrid executable
+2. Run ./2dGrid to view the arrays in standard out
+3. Run make clean to remove the executable
+4. Repeat to recompile
+
--- lab3/Lab3_2DGrid_Student/myKernel.cu
+++ lab3/Lab3_2DGrid_Student/myKernel.cu
@@ -13,22 +13,58 @@ __global__ void kernel( int *a, int dimx, int dimy )
 // in order to meet the requirements in the write-ups. 
 __global__ void kernel2( int *a, int dimx, int dimy )
 {
+    int ix  = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy  = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (ix < dimx && iy < dimy) {
+        int idx = iy * dimx + ix;
+        a[idx]  = blockIdx.y * gridDim.x + blockIdx.x;
+    }
 }
+
 
 __global__ void kernel3( int *a, int dimx, int dimy )
 {
+    int ix  = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy  = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (ix < dimx && iy < dimy) {
+        int idx = iy * dimx + ix;   
+        a[idx]  = idx;
+    }
 }
 
 __global__ void kernel4( int *a, int dimx, int dimy )
 {
+    int ix  = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy  = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (ix < dimx && iy < dimy) {
+        int idx = iy * dimx + ix;
+        int tid = threadIdx.y * blockDim.x + threadIdx.x;
+        a[idx]  = tid;
+    }
 }
 
 __global__ void kernel5( int *a, int dimx, int dimy )
 {
+    int ix  = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy  = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (ix < dimx && iy < dimy) {
+        int idx = iy * dimx + ix;
+        a[idx]  = blockIdx.y;
+    }
 }
 
 __global__ void kernel6( int *a, int dimx, int dimy )
 {
-}
+    int ix  = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy  = blockIdx.y * blockDim.y + threadIdx.y;
 
+    if (ix < dimx && iy < dimy) {
+        int idx = iy * dimx + ix;
+        a[idx]  = blockIdx.x;
+    }
+}
 
