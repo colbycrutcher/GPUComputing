491bfe265696a943c1e638cc3d7b2ec3ac87d57a
benFoster1234567 <156257805+benFoster1234567@users.noreply.github.com>
2026-03-17 02:25:00 -0700

    Added helper templates
    printArrayTo, shuffleArray

---
+++ FinalProj/util.h
@@ -0,0 +1,27 @@
+//
+// Created by ben on 3/16/2026.
+//
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <iterator>
+
+template<typename T>
+void shuffleArray(T* array, int n)
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::shuffle(array, array + n, gen);
+}
+
+template<typename T>
+void printArrayTo(std::ostream &os, T* array, int n)
+{
+	os << "[ ";
+	for (int i = 0; i < n ; i++)
+	{
+		os << array[i] << " ";
+	}
+	os << "]\n";
+}
+
