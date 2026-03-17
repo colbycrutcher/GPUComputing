8c310568a6c82689790873eaaa92944b24dc4ff7
benFoster1234567 <156257805+benFoster1234567@users.noreply.github.com>
2026-03-17 02:23:58 -0700

    basic merge kernel, updated main, added makefile

---
+++ FinalProj/kernel.cuh
@@ -0,0 +1,115 @@
+//
+// Created by ben on 3/16/2026.
+//
+
+#ifndef FINALPROJ_GPUMERGESORT_H
+#define FINALPROJ_GPUMERGESORT_H
+
+
+template<uint sortDir, typename T> __device__ uint binarySearchExclusive(uint val, uint *data, uint lo, uint hi)
+{
+	//ascending order:
+	while (lo < hi)
+	{
+		uint mid = (lo + hi) >> 1;
+		if ((sortDir && data[mid] < val) || (!sortDir && data[mid] > val)) // can probably make this branchless?
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+	return lo;
+}
+
+template<uint sortDir, typename T> __device__ uint binarySearchInclusive(uint val, uint *data, uint lo, uint hi)
+{
+	//ascending order:
+	while (lo < hi)
+	{
+		uint mid = (lo + hi) >> 1;
+		if ((sortDir && data[mid] <= val) || (!sortDir && data[mid] >= val)) // can probably make this branchless?
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+	return lo;
+}
+
+
+template<uint sortDir, typename T>
+__device__
+uint binarySearchExclusive(T key, T *keys, uint lo, uint hi)
+{
+	//ascending order:
+	while (lo < hi)
+	{
+		uint mid = (lo + hi) >> 1;
+		if ((sortDir && keys[mid] < key) || (!sortDir && keys[mid] > key)) // can probably make this branchless later?
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+	return lo;
+}
+
+template<uint sortDir, typename T>
+ __device__
+uint binarySearchInclusive(T key, T *keys, uint lo, uint hi)
+{
+	//ascending order:
+	while (lo < hi)
+	{
+		uint mid = (lo + hi) >> 1;
+		if ((sortDir && keys[mid] <= key) || (!sortDir && keys[mid] >= key))
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+	return lo;
+}
+
+
+
+//must be called log2n times in order to fully merge
+template<uint sortDir, typename T>
+__global__
+void mergeSortedTilesKernel(T *dkey, uint *dval, T* skey, uint *sval, int N, int tileSize)
+{
+	//maybe try and cut this up so it uses shared memory
+	//we calculate the ranks for 2 elements per thread, meaning we only use half of the required threads.
+	//tiles are pretty much seperate from the blocksize, so if we use shared memory, we should be able to convert this pretty easily
+	int gx = blockDim.x * blockIdx.x + threadIdx.x;
+	int tx = threadIdx.x;
+	int tilePairIdx = (gx / (tileSize * 2));
+	int pairPos = gx & (tileSize * 2 - 1);
+
+	int indexInTile = gx & (tileSize - 1);
+
+	int startTileA = (tilePairIdx * tileSize * 2);
+	int startTileB = (tilePairIdx * tileSize * 2 + tileSize);
+
+	T *a_ptr = skey + startTileA;//moves pointer to start of the first tile
+	T *b_ptr = skey + startTileB;//moves pointer to the start of the second tile
+
+	//rank(ai, C) = tileIndex + rank(ai, B) where rank(ai, B) is simply the count of elements bj in B with bj < ai
+	// so I guess rank(ai, B) is the index of where ai would be if it were in b. We do this here with both a and b
+	// to save threads
+
+	uint lo = 0; uint hi = tileSize;
+	uint rankA = indexInTile + binarySearchInclusive<sortDir, T>(*(a_ptr+indexInTile), b_ptr, lo , hi);
+	uint rankB = indexInTile + binarySearchExclusive<sortDir, T>(*(b_ptr+indexInTile), a_ptr, lo , hi);
+
+	int indexA = rankA + startTileA;
+	int indexB = rankB + startTileA;
+
+	int globalIndexA = indexInTile + startTileA;
+	int globalIndexB = indexInTile + startTileB;
+
+	dkey[indexA] = *(a_ptr + indexInTile); // + 2 more global mem accesses
+	dkey[indexB] = *(b_ptr + indexInTile);
+	dval[indexA] = sval[globalIndexA];
+	dval[indexB] = sval[globalIndexB];
+	//4 + log(N)*2 global memory accesses
+}
+
+
+#endif //FINALPROJ_GPUMERGESORT_H
--- FinalProj/main.cu
+++ FinalProj/main.cu
@@ -1,47 +1,119 @@
 // Main CUDA program
 #include <iostream>
-#include "kernel.cu"
+//#include "kernel.cu"
+#include "kernel.h"
+#include "util.h"
 
 // Follow steps of last lab with in the main
 //Have both kernels write to a output file where it displays the differences between the speed of the two kernels.
-int main() {
-    const int N = 1 << 20; // Size of the array
-    float *h_a = new float[N];
-    float *h_b = new float[N];
-    float *d_a, *d_b;
 
-    // Initialize host arrays
-    for (int i = 0; i < N; ++i) {
-        h_a[i] = static_cast<float>(i);
-        h_b[i] = static_cast<float>(i * 2);
-    }
+
+template<uint sortDir, typename T>
+void populateArrayWithDummyPreSortedTilesForTestingTheMergeKernel(T *key, uint *val, int tileSize, int numTiles, int N)
+{
+	//creates a mergable array
+
+	if (sortDir == 1)
+	    for ( int i = 0, j = 0; i < N; i++)
+	    {
+	    	if (i % tileSize == 0) j++;
+	    	int stride = numTiles;
+	    	key[i] = j + (i % tileSize)*stride;// 0 3 6 9 1 4 7 10 ...
+	    	val[i] = static_cast<uint>(i);
+	    }
+
+	else
+        for (int i = 0, j = 0; i < N; i++)
+        {
+            if (i % tileSize == 0) j++;
+            int stride = numTiles;
+            int localIdx = i % tileSize;
+            int reversedIdx = tileSize - 1 - localIdx;
+
+            key[i] = j + reversedIdx * stride;
+            val[i] = static_cast<uint>(i);
+        }
+}
+
+
+int main() {
+    const int N = 1 << 10; // Size of the array
+
+	//host keys (data to sort) and values (index to where they are sorted from)
+	float *h_key = new float[N];
+	uint *h_val = new uint[N];
+
+	//source keys and source values for the GPU
+	float *d_dkey, *d_skey;
+	uint *d_dval, *d_sval;
+
+    // Initialize host arrays and prepare kernel args
+    int blockSize = 256;
+ 	int numBlocks = (N + blockSize - 1) / blockSize;
+
+	if ( N % numBlocks != 0)
+	{
+		std::cerr << "bad input. numBlocks must be a factor of N!\n";
+		exit(1);
+	}
+
+	populateArrayWithDummyPreSortedTilesForTestingTheMergeKernel<1U, float>(h_key, h_val, blockSize, numBlocks, N);
 
     // Allocate device memory
-    cudaMalloc(&d_a, N * sizeof(float));
-    cudaMalloc(&d_b, N * sizeof(float));
+	cudaMalloc(&d_dkey, sizeof(float) * N);
+	cudaMalloc(&d_skey, sizeof(float) * N);
+	cudaMalloc(&d_dval, sizeof(uint) * N);
+	cudaMalloc(&d_sval, sizeof(uint) * N);
 
     // Copy data from host to device
-    cudaMemcpy(d_a, h_a, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_skey, h_key, sizeof(float)*N, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_sval, h_val, sizeof(uint)*N, cudaMemcpyHostToDevice);
 
-    // Launch kernel
-    int blockSize = 256;
-    int numBlocks = (N + blockSize - 1) / blockSize;
-    kernel<<<numBlocks, blockSize>>>(d_a, d_b, N);
+	std::cout << "\nunsorted keys\n";
+	printArrayTo<float>(std::cout, h_key, N);
+	std::cout << "\nunsorted values\n";
+	printArrayTo<uint>(std::cout, h_val, N);
+
+	//initial sort
+
+    // Merge after done sorting
+
+	int tileSize = blockSize;
+
+	for (int l = 0 ; tileSize <= N; l++)
+	{
+		mergeSortedTilesKernel<1U><<<numBlocks, blockSize>>>(d_dkey, d_dval,d_skey, d_sval, N, tileSize);
+
+		//ping ponging between dest and source arrays
+		uint *tempVal = d_dval;
+		d_dval = d_sval;
+		d_sval = tempVal;
+
+		float *tempKey = d_skey;
+		d_skey = d_dkey;
+		d_dkey = tempKey;
+
+		tileSize *= 2;
+	}
 
     // Copy result back to host
-    cudaMemcpy(h_a, d_a, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_key, d_dkey, sizeof(float)*N, cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_val, d_dval, sizeof(uint)*N, cudaMemcpyDeviceToHost);
 
     // Output results    std::cout << "First 10 results: " << std::endl;
-    for (int i = 0; i < 10; ++i) {
-        std::cout << h_a[i] << " ";
-    }
-    std::cout << std::endl;
+	std::cout << "\nsorted keys\n";
+	printArrayTo<float>(std::cout, h_key, N);
+
+	std::cout << "\nsorted values\n";
+	printArrayTo<uint>(std::cout, h_val, N);
 
     // Clean up
-    cudaFree(d_a);
-    cudaFree(d_b);
-    delete[] h_a;
-    delete[] h_b;   
+    cudaFree(d_dkey);
+    cudaFree(d_skey);
+    cudaFree(d_dval);
+    cudaFree(d_sval);
+    delete[] h_key;
+    delete[] h_val;
     return 0;
 }
+
